bc44fbc95fe5fc14e6fb235e99aa8bba844e94e2
Alfiya6282823310 <knalfiya@gmail.com>
2024-06-13 21:48:37 +0530

    search  semi dynamic

---
+++ src/Components/Search.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const Search = () => {
+const [data,changeData]=useState(
+    {
+        "name":""
+    }
+)
+const inputHandler=(event)=>{
+    changeData({...data,[event.target.name]:event.target.value})
+}
+const readValue=()=>{
+    console.log(data)
+}
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-d-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Patient Name</label>
+                            <input type="text" className="form-control" name='name' value={data.name} onClick={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-info" onClick={readValue}>Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Search
