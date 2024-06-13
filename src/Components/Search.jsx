ec1aa9d913199bab23a44f3a78fe59a50becf1a1
Alfiya6282823310 <knalfiya@gmail.com>
2024-06-13 21:55:13 +0530

    search and add semi dynamic finished

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -20,9 +20,9 @@ const readValue=()=>{
                     <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Patient Name</label>
-                            <input type="text" className="form-control" name='name' value={data.name} onClick={inputHandler}/>
+                            <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler}/>
                         </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <button className="btn btn-info" onClick={readValue}>Search</button>
                         </div>
                     </div>
